7bc643135904a3ec50eeddb7af929eb4bc7cc5f6
Narrell <99311885+Narrelln@users.noreply.github.com>
2022-03-13 22:47:19 +0100

    my task for 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: retun 1 after program execution
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
