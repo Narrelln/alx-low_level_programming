e0a6319cb1e2d656f03f04f6718a7761af361a89
Narrell <99311885+Narrelln@users.noreply.github.com>
2022-03-13 22:45:03 +0100

    my 4-puts commit task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
-**
- * main - Entry point
+#include <stdio.h>
+/**
+ *main -  entry point
  *
- * Return: 0 after printing the function
+ *Return: 0 after printing the function
  */
-int  main(void)
-{puts ("\"Programming is like building a multilingual puzzle"):
-return (0):
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
