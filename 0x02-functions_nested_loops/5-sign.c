4fdc562f63d6229ede7db647d346f8f9882994e2
Narrell <99311885+Narrelln@users.noreply.github.com>
2022-03-17 00:27:34 +0100

    my 2nd commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+ /**
+  * main - check the code.
+  * print_sign - return 0 letter not lowercase, 1 letter lowercase
+  *
+  *@n: the int to print
+  * Return: Always 0.
+  */
+ int print_sign(int n)
