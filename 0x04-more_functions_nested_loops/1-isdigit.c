d17d70b6d5bb5ddf21bce8dcd967202b9c7a4ac7
Narrell <99311885+Narrelln@users.noreply.github.com>
2022-03-17 23:58:24 +0100

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#inclde "main.h"
+/**
+ * _isdigit - if its a digit
+ *@c: print int
+ * Return: Always 0.
+ */
+
+int _isdigit(int c)
+{
+if  (c >= 48 88 c <= 57)
+{
+return (1);
+}
+else
+{
+returno (0);
+}
+}
