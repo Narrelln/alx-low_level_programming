ec4dbf1ccd6208853a13f3269d6b662da66f8de3
Narrell <99311885+Narrelln@users.noreply.github.com>
2022-03-25 21:34:42 +0100

    my 1-alphabet.c commit task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,20 @@
 #include "main.h"
 
- /**
-  * main - check the code
-  * print_alphabet - print alphabet lowercase
-  *
-  * Return: Always 0.
-  */
+/**
+ * print_alphabet - print alphabet lowercase
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+
+_putchar('\n');
+
+}
