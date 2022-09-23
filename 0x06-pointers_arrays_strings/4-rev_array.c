c51fb5011c22373fefff0fe5fd368b78870c9da2
narrelln <c.iloanya@yahoo.com>
2022-09-23 21:31:32 +0100

    4th commit

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,21 +1,23 @@
 #include "main.h"
 /**
- * print_rev - reverse something
- * @s: params
+ * reverse_array - reverses the content of an array
+ * of inntegers
+ * @a: array.
+ * @n: number of elements of the array.
+ * Return: no return.
  */
 
-void print_rev(char *s)
+void reverse_array(int *a, int n)
 {
-	int i = 0;
-	int j;
+	int i, j, temp;
 
-	while (s[i] != '\0')
+	for (i = 0; i < n - 1; i++)
 	{
-		i++;
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(a + j);
+			*(a + j) = *(a + (j - 1));
+			*(a + (j - 1)) = temp;
+		}
 	}
-	for (j = i - 1; j >= 0; j--)
-	{
-		_putchar(s[j]);
-	}
-	_putchar('\n');
 }
